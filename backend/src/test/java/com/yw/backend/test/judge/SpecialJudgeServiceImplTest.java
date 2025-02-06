437e538f24f6ce10d2e16b13f929839e846ed55d
ironQHZ <qiuhanzeng@outlook.com>
2025-02-05 19:40:47 -0500

    modify test

--- backend/src/test/java/com/yw/backend/test/judge/SpecialJudgeServiceImplTest.java
+++ backend/src/test/java/com/yw/backend/test/judge/SpecialJudgeServiceImplTest.java
@@ -17,7 +17,7 @@ import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-//Test for specialjudgeserviceimpltest
+//Test for SpecialJudgeServiceImplTest
 @SpringBootTest
 public class SpecialJudgeServiceImplTest {
 
