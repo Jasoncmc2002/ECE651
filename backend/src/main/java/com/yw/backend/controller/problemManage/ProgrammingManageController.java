32d4678987a5c3a47a4f8983bf826b977b83ae83
cmc <2634622798@qq.com>
2025-02-26 11:37:34 -0500

    Modify gitignore

--- backend/src/main/java/com/yw/backend/controller/problemManage/ProgrammingManageController.java
+++ backend/src/main/java/com/yw/backend/controller/problemManage/ProgrammingManageController.java
@@ -86,4 +86,64 @@ public class ProgrammingManageController {
         }
         return programmingManageService.getOne(programmingId);
     }
+    @PostMapping("/problem_manage/programming_manage/")
+    public Map<String, String> create(@RequestParam Map<String, String> data) {
+        String pDescription = data.get("pDescription");
+
+        int pTotalScore;
+        try {
+            pTotalScore = Integer.parseInt(data.get("pTotalScore"));
+        } catch (NumberFormatException e) {
+            Map<String, String> resp = new HashMap<>();
+            resp.put("error_message", "the question score must be a positive integer");
+            return resp;
+        }
+
+        int timeLimit;
+        try {
+            timeLimit = Integer.parseInt(data.get("timeLimit"));
+        } catch (NumberFormatException e) {
+            Map<String, String> resp = new HashMap<>();
+            resp.put("error_message", "the time limit must be a positive integer");
+            return resp;
+        }
+
+        int codeSizeLimit;
+        try {
+            codeSizeLimit = Integer.parseInt(data.get("codeSizeLimit"));
+        } catch (NumberFormatException e) {
+            Map<String, String> resp = new HashMap<>();
+            resp.put("error_message", "the code length limit must be a positive integer");
+            return resp;
+        }
+
+        String pTag = data.get("pTag");
+        String pTitle = data.get("pTitle");
+        String pJudgeCode = data.get("pJudgeCode");
+
+        int pDifficulty;
+        try {
+            pDifficulty = Integer.parseInt(data.get("pDifficulty"));
+        } catch (NumberFormatException e) {
+            Map<String, String> resp = new HashMap<>();
+            resp.put("error_message", "the difficulty coefficient must be a positive integer");
+            return resp;
+        }
+
+        return programmingManageService.create(pDescription, pTotalScore, timeLimit, codeSizeLimit, pTag, pTitle, pJudgeCode, pDifficulty);
+    }
+
+    @DeleteMapping("/problem_manage/programming_manage/")
+    public Map<String, String> delete(@RequestParam Map<String, String> data) {
+        int programmingId;
+        try {
+            programmingId = Integer.parseInt(data.get("programmingId"));
+        } catch (NumberFormatException e) {
+            Map<String, String> resp = new HashMap<>();
+            resp.put("error_message", "invalid programming question ID");
+            return resp;
+        }
+
+        return programmingManageService.delete(programmingId);
+    }
 }
