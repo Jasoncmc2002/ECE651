4b2bc383284c12d57f62aa12c092cbd083c2a9ac
cmc <2634622798@qq.com>
2025-03-04 16:07:45 -0500

    Add features from dev1

--- backend/src/main/java/com/yw/backend/service/setManage/SetManageService.java
+++ backend/src/main/java/com/yw/backend/service/setManage/SetManageService.java
@@ -5,6 +5,18 @@ import java.util.List;
 import java.util.Map;
 
 public interface SetManageService {
+    Map<String, String> create(String psName, LocalDateTime psStartTime, LocalDateTime psEndTime, int duration);
+
+    Map<String, String> delete(int problemSetId);
+
+    List<Map<String, String>> searchObjectiveProblem(int problemSetId, String opDescription, String opTag, int opDifficultyMin, int opDifficultyMax);
+
+    Map<String, String> addObjectiveProblem(int problemSetId, int objectiveProblemId);
+
+    Map<String, String> deleteObjectiveProblem(int problemSetId, int objectiveProblemId);
+
+    List<Map<String, String>> getAddedObjectiveProblem(int problemSetId);
+
     Map<String, String> update(int problemSetId, String psName, LocalDateTime psStartTime, LocalDateTime psEndTime, int duration);
 
     List<Map<String, String>> searchProgramming(int problemSetId, String pTitle, String pTag, int pDifficultyMin, int pDifficultyMax);
