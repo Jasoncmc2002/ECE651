015a3ecf47873b15688af59b91823465fcc8b5f0
Michael <yulin.wu297@outlook.com>
2025-03-15 20:18:26 -0400

    ADD PROBLEM SET OBJECTIVE PROBLEM TEACHER PAGE TO ROUTER

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -37,6 +37,7 @@ import ProblemSetObjectiveProblemStudent from './components/problem_set/student/
 import ProblemSetProgrammingStudent from './components/problem_set/student/ProblemSetProgrammingStudent';
 import ProblemSetAllRecordTeacher from './components/problem_set/teacher/ProblemSetAllRecordTeacher';
 import ProblemSetOneRecordTeacher from './components/problem_set/teacher/ProblemSetOneRecordTeacher';
+import ProblemSetObjectiveProblemTeacher from './components/problem_set/teacher/ProblemSetObjectiveProblemTeacher';
 
 
 
@@ -122,6 +123,7 @@ class App extends Component {
                     <Route path='/problem_set/student_view/programming/:problem_set_id/:programming_id/' element={<ProblemSetProgrammingStudent />} />
                     <Route path='/problem_set/teacher_view/all_record/:problem_set_id/' element={<ProblemSetAllRecordTeacher />} />
                     <Route path='/problem_set/teacher_view/one_record/:problem_set_id/:student_id/' element={<ProblemSetOneRecordTeacher />} />
+                    <Route path='/problem_set/teacher_view/objective_problem/:problem_set_id/:objective_problem_id/:student_id/' element={<ProblemSetObjectiveProblemTeacher />} />
 
 
                     {/* Problem Set Management */}
